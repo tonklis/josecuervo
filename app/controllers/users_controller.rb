7d49cb0ac73c0f7fe3b764b1bfb9650a8d6a6ae0
tonklis <benjamin.hernandez@in2teck.com>
2012-06-22 14:17:10 -0500

    Se agregaron cambios de orden para lista de usuarios en produccion e ignorar folders de imagenes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.where("avatar_file_name is not null").order("created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
